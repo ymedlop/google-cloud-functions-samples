a44954beb562dd480b47ece5b26cd7287c284e28
Yeray Medina López <ymedlop@gmail.com>
2017-06-28 23:25:57 +0200

    delete api key use and use credentials default

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# google-cloud-functions-samples
+google-cloud-functions-samples
+-------------------------------
+
+Working in progress....
+
+## Before you begin
+
+1. Select or create a Cloud Platform project.
+2. Enable billing for your project.
+3. Enable the Cloud Functions and Compute Engine API.
+4. Install and initialize the Cloud SDK.
+5. Update and install gcloud components
+6. Prepare your environment for Node.js development.
--- vpn/index.js
+++ vpn/index.js
@@ -1,22 +1,26 @@
 'use strict';
 
-const config = require('./secrets/config.json');
 const google = require('googleapis');
-const compute = google.compute('v1');
 
 exports.get = function get (req, res) {
-  const request = {
-     project: config.PROJECT,
-     region: 'us-central1',
-     vpnTunnel: 'wito',
-     auth: config.COMPUTE_API_KEY
-   };
-  compute.vpnTunnels.get(request, function(err, response) {
-     if (err) {
-       console.log(err);
-       return;
-     }
-     console.log(JSON.stringify(response, null, 2));
-   });
-  res.status(200).send({});
+  google.auth.getApplicationDefault(function (err, authClient, projectId) {
+    if (err) {
+      throw err;
+    }
+    if (authClient.createScopedRequired && authClient.createScopedRequired()) {
+      authClient = authClient.createScoped([
+        'https://www.googleapis.com/auth/compute'
+      ]);
+    }
+    const compute = google.compute({
+      version: 'v1',
+      auth: authClient
+    });
+    compute.zones.list({
+      project: projectId,
+      auth: authClient
+    }, function (err, result) {
+      res.status(200).send(result);
+    });
+  });
 };
