fe4fe349a9ff3ba4f4a960df72ffc10407546511
Yeray Medina Lopez <ymedlop@gmail.com>
2017-06-29 13:39:28 +0200

    Update with config file

--- vpn/index.js
+++ vpn/index.js
@@ -1,26 +1,73 @@
 'use strict';
 
-const google = require('googleapis');
+// TODO: Errors handlers
 
-exports.get = function get (req, res) {
-  google.auth.getApplicationDefault(function (err, authClient, projectId) {
-    if (err) {
-      throw err;
-    }
+const google = require('googleapis');
+const compute = google.compute('v1');
+const config = require('./config.json');
+
+const authorize = (callback) => {
+  google.auth.getApplicationDefault((err, authClient, projectId) => {
+    if (err) throw err;
     if (authClient.createScopedRequired && authClient.createScopedRequired()) {
-      authClient = authClient.createScoped([
+      authClient.createScoped([
+        'https://www.googleapis.com/auth/cloud-platform',
         'https://www.googleapis.com/auth/compute'
       ]);
     }
-    const compute = google.compute({
-      version: 'v1',
-      auth: authClient
+    callback(authClient, projectId);
+  });
+};
+
+const createTunnels = (baseRequest, vpnTunnels) => {
+  // The tunnel is responsible for encrypting and decrypting traffic exiting
+  // and leaving its associated gateway
+  vpnTunnels.forEach((vpnTunnel) => {
+    const request = Object.assign({}, baseRequest, { resource: vpnTunnel });
+    compute.vpnTunnels.insert(request, (err, result) => {
+      // Each route tells the associated network to send all traffic in the dest_range
+      // through the VPN tunnel
+      if (result) {
+        vpnTunnel.routes.forEach((route) => {
+          const request = Object.assign({}, baseRequest, {resource: route});
+          compute.routes.insert(request);
+        });
+      }
     });
-    compute.zones.list({
+  });
+};
+
+exports.get = (req, res) => {
+  authorize((authClient, projectId) => {
+    const testVpnConfig = config['test-vpn'];
+    const baseRequest = {
+      region: testVpnConfig.region,
       project: projectId,
       auth: authClient
-    }, function (err, result) {
-      res.status(200).send(result);
+    };
+    // Check if a VPN gateway exists
+    const request = Object.assign({}, baseRequest, { targetVpnGateway: testVpnConfig.resource.name });
+    compute.targetVpnGateways.get(request, (err, result) => {
+      if (result) {
+        // TODO: Delete Tunnels
+        // TODO: Create Tunnels
+      } else {
+        // Attach a VPN gateway to the network.
+        const request = Object.assign({}, baseRequest, { resource: testVpnConfig.resource });
+        compute.targetVpnGateways.insert(request, (err, result) => {
+          if (result) {
+            // The following sets of forwarding rules are used as a part of the IPSec protocol
+            testVpnConfig.forwardingRules.UDP.forEach((forwardingRule) => {
+              const request = Object.assign({}, baseRequest, { resource: forwardingRule });
+              compute.forwardingRules.insert(request);
+            });
+            // Forward IPSec traffic coming into our static IP to our VPN gateway.
+            const request = Object.assign({}, baseRequest, { resource: testVpnConfig.forwardingRules.ESP });
+            compute.forwardingRules.insert(request, (err, result) => createTunnels(baseRequest, testVpnConfig.vpnTunnels));
+          }
+        });
+      }
     });
+    res.status(200).send({});
   });
 };
